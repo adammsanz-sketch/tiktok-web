612ccb58a2571f458faa16f3b880e7d8e57fa1ba
adamm sanz <adammsanz@gmail.com>
2025-11-17 21:19:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7916761390-54a84",
+  "appId": "1:184131498048:web:109049d7327cff361cafa5",
+  "apiKey": "AIzaSyBVN6mUqb6mMo52AB0p9AiIPjo6v6qTZxc",
+  "authDomain": "studio-7916761390-54a84.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "184131498048"
+};
