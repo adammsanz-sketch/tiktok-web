2d9c70576664df7d16c1ed60dba4f582bf99daa7
adamm sanz <adammsanz@gmail.com>
2025-11-17 21:12:18 +0000

    WOI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       </Sidebar>
       <SidebarInset>
         <Header />
-        <main className="flex-1 p-4 sm:p-6 lg:p-8">
-          <div className="container mx-auto">
+        <main className="flex flex-1 flex-col items-center justify-center p-4 sm:p-6 lg:p-8">
+          <div className="text-center">
             <h1 className="text-4xl font-bold">Welcome to Sanztech</h1>
             <p className="text-muted-foreground">Select a page from the sidebar to get started.</p>
           </div>
